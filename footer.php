53985439543b0a0cfd3a9053b67bcae42a952299
Tom Woodward <twwoodward@me.com>
2016-12-27 22:45:14 -0500

    footer change, remove titles from single

---
+++ footer.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package understrap
+ */
+
+$the_theme = wp_get_theme();
+$container = get_theme_mod( 'understrap_container_type' );
+?>
+
+<?php get_sidebar( 'footerfull' ); ?>
+
+<div class="wrapper" id="wrapper-footer">
+
+	<div class="<?php echo esc_html( $container ); ?>" id="content">
+
+		<div class="row">
+
+			<div class="col-md-12 footer">
+
+				<footer class="site-footer" id="colophon" role="contentinfo">
+
+					<div class="site-info">
+					Advanced Learning Partnerships<br>
+					Street Address<br>
+					City, North Carolina
+						
+					</div><!-- .site-info -->
+
+				</footer><!-- #colophon -->
+
+			</div><!--col end -->
+
+		</div><!-- row end -->
+
+	</div><!-- container end -->
+
+</div><!-- wrapper end -->
+
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+
+</html>
---
+++ loop-templates/content-single.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Single post partial template.
+ *
+ * @package understrap
+ */
+
+?>
+<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+
+	<!-- <?php echo get_the_post_thumbnail( $post->ID, 'large' ); ?> -->
+
+	<div class="entry-content">
+
+		<?php the_content(); ?>
+
+		<?php
+		wp_link_pages( array(
+			'before' => '<div class="page-links">' . __( 'Pages:', 'understrap' ),
+			'after'  => '</div>',
+		) );
+		?>
+
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+
+		<?php understrap_entry_footer(); ?>
+
+	</footer><!-- .entry-footer -->
+
+</article><!-- #post-## -->
